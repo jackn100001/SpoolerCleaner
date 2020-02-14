b98db8a09ccade8ffc5e08b343361faf2409c3f2
l30n1d45 <l30n1d45@users.noreply.github.com>
2020-02-14 19:26:53 +0000

    Initial commit

---
+++ spooler-cleaner.ps1
@@ -0,0 +1,16 @@
+﻿param (
+    [int]$age = 15
+)
+
+Write-Output "Stopping print spooler..."
+Stop-Service -Name Spooler -Force
+
+$limit = (Get-Date).AddDays($age)
+$path = "C:\Users\jack-\Desktop\Test"
+
+# Delete files older than the $limit.
+Write-Output "Deleting spooler files created $($age) or more days ago"
+Get-ChildItem -Path $path -Recurse -Force | Where-Object { $_.CreationTime -lt $limit -and !$_.PSIsContainer } | Remove-Item -Force
+
+Write-Output "Starting print spooler..."
+Start-Service -Name Spooler
