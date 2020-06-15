1d564d924948149b712c58ea01d7e8a7abcfb680
l30n1d45 <l30n1d45@users.noreply.github.com>
2020-06-15 12:52:44 +0100

    Add path as a parameter

--- spooler-cleaner.ps1
+++ spooler-cleaner.ps1
@@ -1,12 +1,12 @@
 ﻿param (
     [int]$age = 15
+    $path = 'C:\Windows\System32\spool\PRINTERS'
 )
 
 Write-Output "Stopping print spooler..."
 Stop-Service -Name Spooler -Force
 
 $limit = (Get-Date).AddDays($age)
-$path = "C:\Users\jack-\Desktop\Test"
 
 # Delete files older than the $limit.
 Write-Output "Deleting spooler files created $($age) or more days ago"
